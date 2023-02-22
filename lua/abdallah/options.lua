796ef4f0e1b515f3e5ea8f6090c4eecc5577173e
Abdallah Mehiz <AbdallahMehiz@hotmail.com>
2023-02-22 21:48:40 +0100

    use a loop instead

--- lua/abdallah/options.lua
+++ lua/abdallah/options.lua
@@ -1,30 +1,37 @@
 -- Basic Neovim options, use ':help options' for more info
+local options = {
 -- vim.opt.backup = false 		-- Create backup file
-vim.opt.clipboard = "unnamedplus"	-- Use system clipboard
-vim.opt.cmdheight = 2			-- More space in nvim commandline for better output readability
-vim.opt.conceallevel = 0		-- Show text inside `` in markdown
-vim.opt.fileencoding = "utf-8"		-- Set file encoding to utf-8
-vim.opt.hlsearch = true			-- Highlight text searches
-vim.opt.ignorecase = true		-- Ignore case when searching
-vim.opt.mouse = a			-- Enable mouse support for all modes
-vim.opt.pumheight = 10			-- Increase popupmenu height
-vim.opt.showmode = true			-- Show current mode
-vim.opt.showtabline = 2			-- Always show tabs
-vim.opt.smartcase = true		-- Smart case
-vim.opt.smartindent = true		-- Smarter indenting
-vim.opt.splitbelow = true		-- Force all horizontal splits to open up below current window
-vim.opt.splitright = true		-- Force all vertical splits to open up to the right of current window
-vim.opt.termguicolors = true		-- set term gui colors
-vim.opt.timeoutlen = 1000		-- time to wait for a mapped sequence to complete
-vim.opt.undofile = true			-- enable persistent undo
-vim.opt.updatetime = 300		-- faster completion (default = 4000)
-vim.opt.writebackup = false		-- if file is being edited by another program do not 
-vim.opt.expandtab = true		-- convert tabs into spaces (literal typing spaces)
-vim.opt.shiftwidth = 2			-- indentations consist of 2 spaces
-vim.opt.tabstop= 2 			-- insert 2 spaces with each tab
-vim.opt.cursorline = true		-- highlight the current line
-vim.opt.number = true			-- add numbered lines
-vim.opt.relativenumber = false		-- use non-relative numbered lines
-vim.opt.numberwidth = 3			-- set number line width to 3 (default = 4)
-vim.opt.signcolumn = "yes"		-- always show the sign column
-vim.opt.wrap = false			-- remove text wrapping for really long lines
+clipboard = "unnamedplus"	-- Use system clipboard
+cmdheight = 2			-- More space in nvim commandline for better output readability
+conceallevel = 0		-- Show text inside `` in markdown
+fileencoding = "utf-8"		-- Set file encoding to utf-8
+hlsearch = true			-- Highlight text searches
+ignorecase = true		-- Ignore case when searching
+mouse = a			-- Enable mouse support for all modes
+pumheight = 10			-- Increase popupmenu height
+showmode = true			-- Show current mode
+showtabline = 2			-- Always show tabs
+smartcase = true		-- Smart case
+smartindent = true		-- Smarter indenting
+splitbelow = true		-- Force all horizontal splits to open up below current window
+splitright = true		-- Force all vertical splits to open up to the right of current window
+termguicolors = true		-- set term gui colors
+timeoutlen = 1000		-- time to wait for a mapped sequence to complete
+undofile = true			-- enable persistent undo
+updatetime = 300		-- faster completion (default = 4000)
+writebackup = false		-- if file is being edited by another program do not 
+expandtab = true		-- convert tabs into spaces (literal typing spaces)
+shiftwidth = 2			-- indentations consist of 2 spaces
+tabstop= 2 			-- insert 2 spaces with each tab
+cursorline = true		-- highlight the current line
+number = true			-- add numbered lines
+relativenumber = false		-- use non-relative numbered lines
+numberwidth = 3			-- set number line width to 3 (default = 4)
+signcolumn = "yes"		-- always show the sign column
+wrap = false			-- remove text wrapping for really long lines
+}
+
+-- loop and set options
+for k, v in pairs(options) do
+vim.opt[k] = v
+end
