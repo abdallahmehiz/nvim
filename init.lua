3d06f856d98aedf016d5b8fb4c781c00e9f53d28
Abdallah Mehiz <Abdallah@Mehiz.xyz>
2023-03-05 23:10:46 +0000

    restarting the whole thing... couldnt get lsps tp work properly for some reason

--- init.lua
+++
@@ -1,5 +0,0 @@
-require "abdallah.options" -- Basic options and settings
-require "abdallah.plugins" -- Plugins
-require "abdallah.cmp"  -- Autocompletion
-require "abdallah.colorscheme"   -- Colorscheme
-require "abdallah.startup"  -- Startup script
